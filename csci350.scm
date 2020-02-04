2092bf79d60855f7a9301a437b2b8220cedf5970
KendalH98 <34657129+KendalH98@users.noreply.github.com>
2020-02-04 16:43:05 -0500

    First Commit
    I almost finished the first function

---
+++ csci350.scm
@@ -0,0 +1,6 @@
+;Function 1 reverses the contents of a list
+(define (reverse-general L)
+  (cond ((null? L) '())
+        ((list? L)
+         (append (reverse-general (cdr L))
+                 (list (reverse-general (car L)))))
